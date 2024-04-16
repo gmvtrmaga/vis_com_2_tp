e1f644e70e78f914b61d9856c0fa890470eded9a
lucasescucha <lucasmatiasbarrera@gmail.com>
2024-04-16 17:48:20 +0200

    Changes for fine tune SqueezeNet

--- src/models/strategies/SqueezeNetModel.py
+++ src/models/strategies/SqueezeNetModel.py
@@ -2,41 +2,27 @@ import torch
 import torchmetrics
 import torchvision
 
-N_LAYERS_TO_KEEP = 8
-
 class CustomSqueezeNet(torch.nn.Module):
-    def __init__(self, image_size: int, *args, **kwargs) -> None:
+    def __init__(self, n_freeze: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.squeezeNet = torchvision.models.squeezenet1_1(
             weights=torchvision.models.SqueezeNet1_1_Weights.DEFAULT
         )
 
-        for param in self.squeezeNet.parameters():
+        for param in list(self.squeezeNet.features.parameters())[:-n_freeze]:
             param.requires_grad = False
-        
-        # Obtiene el tamaño de salida de la última capa que se toma
-        # del modelo
-        y = torch.zeros([1, 3, image_size, image_size])
-        for i in range(N_LAYERS_TO_KEEP):
-            y = self.squeezeNet.features[i].forward(y)
-        out_size = torch.flatten(y, start_dim=1).shape[1]
 
-
-        self.fc1 = torch.nn.Linear(in_features=out_size, out_features=512, bias=True)
-        self.fc2 = torch.nn.Linear(in_features=512, out_features=1, bias=True)
+        self.squeezeNet.classifier[1] = torch.nn.Conv2d(512, 1, kernel_size=(1,1))
+        self.squeezeNet.num_classes = 1
 
     def forward(self, x):
-        for i in range(N_LAYERS_TO_KEEP):
-            x = self.squeezeNet.features[i].forward(x)
-
-        x = torch.relu(self.fc1(torch.flatten(x, start_dim=1)))
-        return torch.sigmoid(self.fc2(x)).flatten()
+        return torch.sigmoid(self.squeezeNet.forward(x)).flatten()
 
 
 class SqueezeNetModelTrainConfig:
-    def __init__(self, image_size: int, lr: float) -> None:
-        self.model = CustomSqueezeNet(image_size)
+    def __init__(self, n_unfreeze: int, lr: float) -> None:
+        self.model = CustomSqueezeNet(n_unfreeze)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
         self.loss = torch.nn.BCELoss()
         self.metrics = {'F1': torchmetrics.F1Score(task='binary'),
